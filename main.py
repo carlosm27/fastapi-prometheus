f81b2f3eb076d104fd062de5f6a8e153b51178ef
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-08-17 21:51:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
+from prometheus_client import make_asgi_app, Counter
 
 app = FastAPI()
+index_counter = Counter('index_counter', 'Description of counter')
 
-from prometheus_client import make_asgi_app, CollectorRegistry, multiprocess
-
-app = FastAPI(debug=False)
-
-# Using multiprocess collector for registry
-
+all_requests = Counter('all_requests', 'A counter of the all requests made')
+all_requests.inc()
 
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
 @app.get("/")
+
 def index():
-  
-   
+    index_counter.inc()
+    
+    
     return "Hello, world!"
   
 
