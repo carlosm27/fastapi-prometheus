2cf3e7899e0c3055e5e80705128c1dec4f533a2f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-08-20 21:52:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from prometheus_client import make_asgi_app, Counter
 
 app = FastAPI()
+
 index_counter = Counter('index_counter', 'Description of counter')
 
 all_requests = Counter('all_requests', 'A counter of the all requests made')
-all_requests.inc()
+
 
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
+
+@app.middleware("tracing")
+def tracing(request: Request, call_next):
+    all_requests.inc()
+    response = call_next(request)
+    return response
 
 @app.get("/")
 
